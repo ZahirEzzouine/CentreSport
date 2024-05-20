21005fdc20117d8282c214cf7da43f5628af54e2
zakben07 <zakbenhamadi7@gmail.com>
2024-05-20 15:45:44 +0000

    commit h

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Produitcontrol;
 use App\Http\Controllers\AuthentificationController;
+use App\Http\Controllers\NavbarController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -30,4 +31,15 @@ Route::post('/check_code_verification',[AuthentificationController::class , 'che
 
 Route::post('/new_password',[AuthentificationController::class ],'new_password');
 
+
+
+
+
+
+
+Route::get('/about',[NavbarController::class ,'about']);
+
+Route::get('/services',[NavbarController::class ,'services']);
+
+Route::get('/contact',[NavbarController::class ,'contact']);
 
